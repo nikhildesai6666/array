3856e62a287045ba6cebd0bf523fbe5f9c994b1f
nikhildesai6666 <desainikhil9664@gmail.com>
2022-10-18 11:47:15 +0530

    commit

---
+++ array/array2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class array2D
+    {
+        static void Main(string[]args)
+        {
+            //2D array ---> accept the number from user
+
+            int[,] data = new int[3, 4];
+                
+                for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; i < data.GetLength(1); j++)
+                {
+                    data[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.WriteLine($"data=[{i},{j}] ={data[i, j]}");
+
+                }
+                Console.WriteLine();
+            
+            
+            
+
+            }
+                   
+
+                
+        }
+    }
+}
---
+++ array/array2D2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class array2D2
+    {
+        static void Main(string[] args)
+        {
+            //2D Array --> putting direct values
+
+            int[,] data = new int[3, 4]
+          {
+                   {10,20,30,40 },
+                   { 50,60,70,80},
+                   { 90,100,110,120}
+          };
+
+            int[,] data2 = new int[4, 6]
+            {
+                { 1,2,3,4,5,6},
+                {1,2,3,4,5,6 },
+                {1,2,3,4,5,6 },
+                { 1,2,3,4,5,6}
+            };
+
+            for (int i = 0; i < data.GetLength(0); i++)   // 0--> row size
+            {
+                for (int j = 0; j < data.GetLength(1); j++) // 1 --> col size
+                {
+                    Console.Write($"data=[{i},{j}] = {data[i, j]}  ");
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
