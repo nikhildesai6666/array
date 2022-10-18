42eb8c523caa5e68f14720d56b00cc3417a48327
nikhildesai6666 <desainikhil9664@gmail.com>
2022-10-18 10:59:41 +0530

    Add project files.

---
+++ array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5] {10,20,30,40,50};
+            double[] rainfall = new double[7] {1,2,3,4,5,6,7};
+            string[] names = new string[3] {"Nikhil","Shubham","Sushant"};
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+
+                numbers[i]=Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+            for (int i = 0; i <names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            for (int i = 0; i <rainfall.Length; i++)
+            {
+                Console.WriteLine(rainfall[i]);
+
+            }
+
+            
+        }
+    }
+}
---
+++ array/foreacharray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class foreacharray
+    {
+        static void Main(string[]args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            double[] rainfall =  { 1, 2, 3, 4, 5, 6, 7 };
+            string[] names =  { "Nikhil", "Shubham", "Sushant" };
+             
+            foreach(int n in numbers)
+            {
+                    Console.WriteLine(n);   
+            }
+            foreach(double r in rainfall)
+            {
+                Console.WriteLine(r);
+
+            }
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
