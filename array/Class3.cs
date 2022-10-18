e70fb74c480e37ca2dc33eea3bdca9e38b84c8cc
nikhildesai6666 <desainikhil9664@gmail.com>
2022-10-18 12:45:36 +0530

    Commit

--- array/Class3.cs
+++ array/Class3.cs
@@ -1,21 +1,21 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace array
-//{
-//    internal class Class3
-//    {
-//        //reverse
-//        static void Main(string[] args)
-//        {
-//            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-//            for (int i = array.Length - 1; i >= 0; i--)
-//            {
-//                Console.WriteLine(array[i]);
-//            }
-//        }
-//    }
-//}
+namespace array
+{
+    internal class Class3
+    {
+        //reverse
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
--- array/foreacharray.cs
+++ array/foreacharray.cs
@@ -4,29 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace array
-//{
-//    internal class foreacharray
-//    {
-//        static void Main(string[]args)
-//        {
-//            int[] numbers = { 10, 20, 30, 40, 50 };
-//            double[] rainfall =  { 1, 2, 3, 4, 5, 6, 7 };
-//            string[] names =  { "Nikhil", "Shubham", "Sushant" };
-             
-//            foreach(int n in numbers)
-//            {
-//                    Console.WriteLine(n);   
-//            }
-//            foreach(double r in rainfall)
-//            {
-//                Console.WriteLine(r);
+namespace array
+{
+    internal class foreacharray
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            double[] rainfall = { 1, 2, 3, 4, 5, 6, 7 };
+            string[] names = { "Nikhil", "Shubham", "Sushant" };
 
-//            }
-//            foreach(string name in names)
-//            {
-//                Console.WriteLine(name);
-//            }
-//        }
-//    }
-//}
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            foreach (double r in rainfall)
+            {
+                Console.WriteLine(r);
+
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
